a8cbf5961e341478b5d92c42e7b6dd7e83e1e06d
Bugord <yury.kakareka@gmail.com>
2023-09-24 19:40:33 +0200

    Added land animation, sync gun pos with player during run animation

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -6,9 +6,7 @@ func _ready():
 	sprite2d.visible = false
 
 func _physics_process (_delta) -> void: 
-	var mouse_pos = get_global_mouse_position();
-	sprite2d.flip_v = mouse_pos.x < get_parent().position.x	 
-	look_at(mouse_pos)
+	look_at(get_global_mouse_position())
 
 func enable_gun():
 	sprite2d.visible = true
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,7 +9,6 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var is_looking_right: bool = true; 
 
 func _ready():
 	animation_tree.active = true
@@ -17,27 +16,34 @@ func _ready():
 func is_walking():
 	return abs(velocity.length()) > 0.1
 
+func _process(_delta):
+	transform.x.x = 1 if is_looking_right() else -1
+
 func _physics_process(delta):
-	# Add the gravity.
+	var direction = Input.get_axis("Left", "Right")
+
+	apply_gravity(delta)
+	jump()
+	move(direction)
+	
+
+	animation_tree["parameters/player_run/TimeScale/scale"] = 1 if is_looking_right() == (direction > 0) else -1
+	move_and_slide()
+	animation_tree["parameters/player_jump/blend_position"] = velocity.y
+
+func jump():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+func apply_gravity(delta: float):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("Left", "Right")
+func move(direction: float):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	is_looking_right = get_global_mouse_position().x > position.x
-	sprite.set_flip_h(!is_looking_right)
-
-	animation_tree["parameters/player_run/TimeScale/scale"] = 1 if is_looking_right == (direction > 0) else -1
-	move_and_slide()
-	animation_tree["parameters/player_jump/blend_position"] = velocity.y
+func is_looking_right():
+	return get_global_mouse_position().x > position.x
