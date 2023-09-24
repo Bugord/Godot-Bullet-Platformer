cb0fbe8ac289340f59a5f0e8f818c2cccfb67cda
Bugord <yury.kakareka@gmail.com>
2023-09-24 02:08:44 +0200

    Added spawning

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -2,7 +2,13 @@ extends Node2D
 
 @onready var sprite2d: Sprite2D = $Sprite2D 
 
+func _ready():
+	sprite2d.visible = false
+
 func _physics_process (_delta) -> void: 
 	var mouse_pos = get_global_mouse_position();
 	sprite2d.flip_v = mouse_pos.x < get_parent().position.x	 
 	look_at(mouse_pos)
+
+func enable_gun():
+	sprite2d.visible = true
