5e55d7e7a624b73a43ae7af10746a8b60ff564b4
Bugord <yury.kakareka@gmail.com>
2023-09-24 20:05:18 +0200

    Added animation for firing

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -1,12 +1,24 @@
 extends Node2D
+class_name Gun
 
-@onready var sprite2d: Sprite2D = $Sprite2D 
+@onready var sprite2d: Sprite2D = $Gun
+@onready var animation_player: AnimationPlayer = $AnimationPlayer 
 
 func _ready():
 	sprite2d.visible = false
+	animation_player.play("gun_idle")
+
+func _process(_delta):
+	fire()
 
 func _physics_process (_delta) -> void: 
 	look_at(get_global_mouse_position())
 
 func enable_gun():
 	sprite2d.visible = true
+
+func fire():
+	if Input.is_action_just_pressed("Fire"):
+		animation_player.stop()
+		animation_player.play("gun_fire");
+
